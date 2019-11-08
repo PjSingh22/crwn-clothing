2f4b4e3ec0fcdd8c6c952bd0f8229b0a54eca794
Prabhjot Singh <pjsingh1359@outlook.com>
2019-11-07 16:13:02 -0800

    updating out shop page, collection preview component, and collection, and collection item component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -3,8 +3,8 @@ import SHOP_DATA from './shop.data.js';
 import CollectionPreview from  '../../components/collection-preview/collection-preview.component';
 
 class shopPage extends React.Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
     this.state = {
       collections: SHOP_DATA
     }
