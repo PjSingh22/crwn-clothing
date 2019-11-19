42e0cc02802f4b1ed0349d2bd0b3ff1b47d19ef1
Prabhjot Singh <pjsingh1359@outlook.com>
2019-11-19 11:07:59 -0800

    finished sign in handleSubmit method

--- src/App.js
+++ src/App.js
@@ -29,10 +29,10 @@ class App extends React.Component {
               id: snapShot.id,
               ...snapShot.data()
             }
-          }, () => console.log(this.state))
+          })
         })
       } else {
-        this.setState({ currentUser: userAuth }, () => console.log(this.state));
+        this.setState({ currentUser: userAuth });
       }
     });
   }
